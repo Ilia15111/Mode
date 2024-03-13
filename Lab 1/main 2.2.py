912e65e21d5dbcd0ed7ca1494571a5d65c253314
Ilia15111 <160257391+Ilia15111@users.noreply.github.com>
2024-03-13 13:59:00 +0300

    Update main 2.2.py

--- Lab 1/main 2.2.py
+++ Lab 1/main 2.2.py
@@ -9,31 +9,28 @@ z_max = 10*(100+4*p+3*q+1)
 i = p + q + 1
 arr = []
 
-def depositFunc(t, z0, i, c0):
+def depositFunc(t, z0, i):
         t += 1
-        c0 += z0*0.1
-        z0 += (z0 * (0.01 * i))+(c0)
-        return z0, t, c0
+        r = i*0.01
+        z0 = z0 + (z0 * r)+t*(y0*0.1)+y0
+        return z0, t
 
-x = depositFunc(t, z0, i, c0)
+x = depositFunc(t, z0, i)
 
 while True:
     if x[1] < 5:
         z0 = x[0]
         t = x[1]
-        c0 = x[2]
-        x = depositFunc(t, z0, i,c0)
+        x = depositFunc(t, z0, i)
         arr.append(x[0])
     elif x[1] == 5:
         if x[0] > (z_max):
-              print("минимальное число ", y0," число ", round(x[0],2))
+              print("минимальное число ", round(y0,2)," число ", round(x[0],2))
               exit()
         else:
-            y0+=1
+            y0+=0.0001
             z0=y0
             c0=y0
             t = 0
-            x = depositFunc(t, z0, i, c0)
+            x = depositFunc(t, z0, i)
             
-
-
