80a6488958b8ad6ed22889ec1eb369dc8c070f0a
Ilia15111 <160257391+Ilia15111@users.noreply.github.com>
2024-03-13 13:48:51 +0300

    Update main2.py

--- Lab 1/main2.py
+++ Lab 1/main2.py
@@ -9,26 +9,25 @@ z0 = y0
 i = p + q + 1
 arr = []
 
-def depositFunc(t, z0, i, c0):
+def depositFunc(t, z0, i):
         t += 1
-        c0 += z0*0.1
-        z0 += (z0 * (0.01 * i))+(c0)
-        return z0, t, c0
+        r = i*0.01
+        z0 = z0 + (z0 * r)+t*(y0*0.1)+y0
+        return z0, t
 
 
 
-x = depositFunc(t, z0, i,c0)
+x = depositFunc(t, z0, i)
 arr.append(x[0])
 print("=====================================================================")
-print(x[1], " | ", f"{round(z0, 2)}+{round(z0 * (0.01 * i), 2)}+{round(x[2],2)}={round(x[0], 2)}")
-while x[0] <= (y0 * 5):
+print(x[1], " | ", f"{round(z0, 2)}+{round(z0 * (0.01 * i), 2)}+{round(x[1],2)}*{round(0.1*y0,2)}+{y0}={round(x[0], 2)}")
+while x[0] <= (y0 * 10):
     z0 = x[0]
     t = x[1]
-    c0 = x[2]
-    x = depositFunc(t, z0, i,c0)
+    x = depositFunc(t, z0, i)
     arr.append(x[0])
     print("=====================================================================")
-    print(x[1], " | ", f"{round(z0, 2)}+{round(z0 * (0.01 * i), 2)}+{round(x[2],2)}={round(x[0], 2)}")
+    print(x[1], " | ", f"{round(z0, 2)}+{round(z0 * (0.01 * i), 2)}+{round(x[1],2)}*{round(0.1*y0,2)}+{y0}={round(x[0], 2)}")
 
 
 
